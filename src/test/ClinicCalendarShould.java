5f77d4003ae1fcefe9c0fce257237e178e91676c
Ravi Shankar Pandey <ravi.pandey@rupeek.com>
2020-04-19 19:42:53 +0530

    Renames class ClinicCalendarTest to ClinicCalendarShould

--- src/test/ClinicCalendarShould.java
+++ src/test/ClinicCalendarShould.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ClinicCalendarTest {
+class ClinicCalendarShould {
     private ClinicCalendar calendar;
     @BeforeAll
     static void testClassSetup(){
